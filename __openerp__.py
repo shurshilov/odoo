a432b1c621bd43ca9b83b94e464334afcd783f85
shurshilov <shurshilov.a@yandex.ru>
2016-08-14 18:46:18 +0300

    Add files via upload

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Attachment files are opened in browser',
+	'version': '0.1',
+	'depends': ['mail'],
+    'author': 'Shurshilov Artem',
+    'category': 'Mail extension',
+    'description': """
+Attachment files are opened in browser
+==================================================================
+ir.attachment files from mail modul are opened in browser'
+    """,
+    'website': 'https://github.com/shurshilov',
+    'data': [    ],
+}
