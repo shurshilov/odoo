619e49f7fa2da480cde8867ae2242df3130d9db3
unknown <shurshilov.a@yandex.ru>
2016-08-26 01:56:55 +0700

    differ moduls

---
+++ mail_attachment_extension/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Attachment browser',
+	'version': '1.0',
+	'depends': ['mail', 'knowledge'],
+    'author': 'Shurshilov Artem',
+    'category': 'Mail and Knowledge extension',
+    'description': """
+Attachment files are opened in browser
+==================================================================
+1. ir.attachment files from mail modul are opened in browser'
+2. ir.attachment files from knowledge modul are opened in browser
+    """,
+    'website': 'https://github.com/shurshilov',
+}
--- mail_message_extension/__openerp__.py
+++ mail_message_extension/__openerp__.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Attachment files are opened in browser',
+    'name': 'Message extension',
 	'version': '0.1',
 	'depends': ['mail'],
     'author': 'Shurshilov Artem',
     'category': 'Mail extension',
     'description': """
-Attachment files are opened in browser
+Link to the author_id reveals not a model res.partner and
+my_mypartner to get in the parameter is not visible link res.partner
+This is security_mod
 ==================================================================
-ir.attachment files from mail modul are opened in browser'
     """,
     'website': 'https://github.com/shurshilov',
     'data': [ 'views/mail.xml',  ],
