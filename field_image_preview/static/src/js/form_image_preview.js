a4644b73a8a20cf8eea7113e46e38f387ae40202
Artem <shurshilov.a@yandex.ru>
2017-10-19 22:49:25 +0300

    add 11.0 ver

--- field_image_preview/static/src/js/form_image_preview.js
+++ field_image_preview/static/src/js/form_image_preview.js
@@ -2,106 +2,114 @@
 odoo.define('field_image_preview.image_widget_extend', function (require) {
 "use strict";
 
-
     var core = require('web.core');
-    var form_common = require('web.form_common');
-    var imageWidget = core.form_widget_registry.get('image');
+    var base_f = require('web.basic_fields')
+    //var imageWidget = core.form_widget_registry.get('image');
+	var imageWidget = base_f.FieldBinaryImage
     var session = require('web.session');
     var utils = require('web.utils');
     var QWeb = core.qweb;
 
 imageWidget.include({
-	
-    render_value: function() {
+
+    _render: function () {
         var self = this;
-        var url;
-        this.session = session;
-        if (this.get('value') && !utils.is_bin_size(this.get('value'))) {
-            url = 'data:image/png;base64,' + this.get('value');
-        } else if (this.get('value')) {
-            var id = JSON.stringify(this.view.datarecord.id || null);
-            var field = this.name;
-            if (this.options.preview_image)
-                field = this.options.preview_image;
-            url = session.url('/web/image', {
-                                        model: this.view.dataset.model,
-                                        id: id,
-                                        field: field,
-                                        unique: (this.view.datarecord.__last_update || '').replace(/[^0-9]/g, ''),
-            });
-        } else {
-            url = this.placeholder;
-        }
-
-       var $img = $(QWeb.render("FieldBinaryImage-img", { widget: this, url: url }));
-       
-       var imgSrc;
-       if (this.get('value') && !utils.is_bin_size(this.get('value'))) {
-            imgSrc = 'data:image/png;base64,' + this.get('value');
-        } else if (this.get('value')) {
-            var id = JSON.stringify(this.view.datarecord.id || null);
-            var field = this.name;
-            if (this.options.preview_image)
-                field = this.options.preview_image;
-            if (field == "image_medium" ||
-                field == "image_small")
-                field = "image";
-
-            imgSrc = session.url('/web/image', {
-                                        model: this.view.dataset.model,
-                                        id: id,
-                                        field: field,
-                                        unique: (this.view.datarecord.__last_update || '').replace(/[^0-9]/g, ''),
-            });
-        } else {
-            imgSrc = this.placeholder;
-        }
-
-        $($img).click(function(e) {
-            if(self.view.get("actual_mode") == "view") {
-                var $button = $(".oe_form_button_edit");
-                $button.openerpBounce();
-                e.stopPropagation();
+        var attrs = this.attrs;
+        var url = this.placeholder;
+        if (this.value) {
+            if (!utils.is_bin_size(this.value)) {
+                url = 'data:image/png;base64,' + this.value;
+            } else {
+                url = session.url('/web/image', {
+                    model: this.model,
+                    id: JSON.stringify(this.res_id),
+                    field: this.nodeOptions.preview_image || this.name,
+                    // unique forces a reload of the image when the record has been updated
+                    unique: (this.recordData.__last_update || '').replace(/[^0-9]/g, ''),
+                });
             }
+        }
+        var $img = $('<img>').attr('src', url);
+        //var $img = $(QWeb.render("FieldBinaryImage-img", { widget: this, url: url }));
+        $img.css({
+            width: this.nodeOptions.size ? this.nodeOptions.size[0] : attrs.img_width || attrs.width,
+            height: this.nodeOptions.size ? this.nodeOptions.size[1] : attrs.img_height || attrs.height,
+        });
+
+        var imgSrc = this.placeholder;
+        if (this.value) {
+            if (!utils.is_bin_size(this.value)) {
+                imgSrc = 'data:image/png;base64,' + this.value;
+            } else {
+                var field = this.nodeOptions.preview_image || this.name;
+                if (field == "image_medium" ||
+                field == "image_small")                
+                field = "image";
+            	console.log(field);
+            	console.log(imgSrc);
+
+
+                imgSrc = session.url('/web/image', {
+                    model: this.model,
+                    id: JSON.stringify(this.res_id),
+                    field: field,
+                    // unique forces a reload of the image when the record has been updated
+                    unique: (this.recordData.__last_update || '').replace(/[^0-9]/g, ''),
+                });
+                console.log(imgSrc);
+            }
+        }
+        
+                //!!!!!!!!!!!!!!
+        $($img).click(function(e) {
+//           if(self.view.get("actual_mode") == "view") {
+       //         var $button = $(".oe_form_button_edit");
+        //        $button.openerpBounce();
+         //       e.stopPropagation();
+        //    }
 
             // set attr SRC image, in our hidden div
-            $('#inner').attr({src: imgSrc});
+            //console.log("bla 2");
+            //$('#inner').attr({src: imgSrc});
+            console.log($('#inner'));            
+            $('#outer').prepend('<img id="inner" src="'+imgSrc+'" />');            
             $('#outer').fadeIn('slow');
     
             $('#outer').click(function(e)
             {
+            	self.$('#inner').remove();
                 $(this).fadeOut();
+
             });
 
            $(document).mouseup(function (e){ // action click on web-document
-             var div = $("#outer"); // ID-element
-             if (!div.is(e.target) // if click NO our elementе
+          	  self.$('#inner').remove();
+              var div = $("#outer"); // ID-element
+              if (!div.is(e.target) // if click NO our elementе
               && div.has(e.target).length === 0) { // and NO our children elemets
-              div.hide(); 
+              div.hide();
+
              }
            });
-			
+            
             
         });
-        
-        this.$el.find('#outer').remove();
-        //this.$el.find('.in').remove();
-        this.$el.find('> img').remove();
+            
+            
+        //!!!!!!!!!!!
+        this.$('#outer').remove();
+        //!!!!!!!!!
+        this.$('> img').remove();
+       // this.$('#inner').remove();
         this.$el.prepend($img);
-        $img.load(function() {
-            if (! self.options.size)
-                return;
-            $img.css("max-width", "" + self.options.size[0] + "px");
-            $img.css("max-height", "" + self.options.size[1] + "px");
-        });
-        $img.on('error', function() {
+        $img.on('error', function () {
             self.on_clear();
             $img.attr('src', self.placeholder);
             self.do_warn(_t("Image"), _t("Could not display the selected image."));
         });
     },
 
-});
 
+});
 
 });
