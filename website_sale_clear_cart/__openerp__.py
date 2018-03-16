a676e194a5aaa53927195403af88ecab417f1787
mega_buster <shurshilov.a@yandex.ru>
2018-03-17 00:36:49 +0300

    [ADD] module clear button v11.0

---
+++ website_sale_clear_cart/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Clear cart button',
+    'version': '1.0.0',
+    'author': 'Artem Shurshilov',
+    'license': 'LGPL-3',
+    'category': 'eCommerce',
+    'website': 'https://vk.com/id20132180',
+    'price': 30.00,
+    'currency': 'EUR',
+    'depends': ['website_sale'],
+    'data': [
+        'website_sale_clear_cart_views.xml',
+    ],
+    'installable': True,
+}
