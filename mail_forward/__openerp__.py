195740a403a92a1ab61c30361f041b4388ac1918
mega_buster <shurshilov.a@yandex.ru>
2018-03-17 01:19:55 +0300

    [ADD] mail forward 9.0

---
+++ mail_forward/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Show forward button""",
+    "summary": """Forward message to other email or partner and user""",
+    "category": "Discuss",
+    "images": [],
+    "version": "1.0.0",
+
+    "author": "Shurshilov Artem",
+    "website": "https://vk.com/id20132180",
+    "license": "LGPL-3",
+    'price': 30.00,
+    "depends": [
+        "mail_base",
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+    "data": [
+        'templates.xml'
+    ],
+    "qweb": [
+        "static/src/xml/forward_button.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}
