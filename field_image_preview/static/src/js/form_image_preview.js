71afaaf74ab8fbc88cdf29988f89ee06572ddfa7
mega_buster <shurshilov.a@yandex.ru>
2019-05-07 15:10:49 +0300

    [ADD] 12 version field i,age preview

--- field_image_preview/static/src/js/form_image_preview.js
+++ field_image_preview/static/src/js/form_image_preview.js
@@ -8,74 +8,79 @@ odoo.define('field_image_preview.image_widget_extend', function (require) {
 	var imageWidget = base_f.FieldBinaryImage
     var session = require('web.session');
     var utils = require('web.utils');
-    var QWeb = core.qweb;
+    var qweb = core.qweb;
     // check bug 17.01.18
     var field_utils = require('web.field_utils');
+    var DocumentViewer = require('mail.DocumentViewer');
 
 imageWidget.include({
 
     _render: function () {
-        var self = this;
-        var attrs = this.attrs;
+                var self = this;
         var url = this.placeholder;
         if (this.value) {
             if (!utils.is_bin_size(this.value)) {
-                url = 'data:image/png;base64,' + this.value;
+                // Use magic-word technique for detecting image type
+                url = 'data:image/' + (this.file_type_magic_word[this.value[0]] || 'png') + ';base64,' + this.value;
             } else {
                 url = session.url('/web/image', {
                     model: this.model,
                     id: JSON.stringify(this.res_id),
                     field: this.nodeOptions.preview_image || this.name,
                     // unique forces a reload of the image when the record has been updated
-                    //unique: (this.recordData.__last_update || '').replace(/[^0-9]/g, ''),
-			// check bug 17.01.18
-	              unique: field_utils.format.datetime(this.recordData.__last_update).replace(/[^0-9]/g, ''),
+                    unique: field_utils.format.datetime(this.recordData.__last_update).replace(/[^0-9]/g, ''),
                 });
             }
         }
-        var $img = $('<img>').attr('src', url);
-        $img.css({
-            width: this.nodeOptions.size ? this.nodeOptions.size[0] : attrs.img_width || attrs.width,
-            height: this.nodeOptions.size ? this.nodeOptions.size[1] : attrs.img_height || attrs.height,
+        var $img = $(qweb.render("FieldBinaryImage-img", {widget: this, url: url}));
+        // override css size attributes (could have been defined in css files)
+        // if specified on the widget
+        var width = this.nodeOptions.size ? this.nodeOptions.size[0] : this.attrs.width;
+        var height = this.nodeOptions.size ? this.nodeOptions.size[1] : this.attrs.height;
+        if (width) {
+            $img.attr('width', width);
+            $img.css('max-width', width + 'px');
+        }
+        if (height) {
+            $img.attr('height', height);
+            $img.css('max-height', height + 'px');
+        }
+        this.$('> img').remove();
+        this.$el.prepend($img);
+        $img.on('error', function () {
+            self._clearFile();
+            $img.attr('src', self.placeholder);
+            self.do_warn(_t("Image"), _t("Could not display the selected image."));
         });
 
         //Odoo save 3 variant of image, just image is original also have medium and small
         var imgSrc = this.placeholder;
         if (this.value) {
             if (!utils.is_bin_size(this.value)) {
-                imgSrc = 'data:image/png;base64,' + this.value;
+                // Use magic-word technique for detecting image type
+                imgSrc = 'data:image/' + (this.file_type_magic_word[this.value[0]] || 'png') + ';base64,' + this.value;
             } else {
-                var field = this.nodeOptions.preview_image || this.name;
-                if (field == "image_medium" ||
-                field == "image_small")                
-                field = "image";
-            	console.log(field);
-            	console.log(imgSrc);
-
-
                 imgSrc = session.url('/web/image', {
                     model: this.model,
                     id: JSON.stringify(this.res_id),
-                    field: field,
+                    field: "image" || this.nodeOptions.preview_image || this.name,
                     // unique forces a reload of the image when the record has been updated
-                   // unique: (this.recordData.__last_update || '').replace(/[^0-9]/g, ''),
-		   // check bug 17.01.18
-	              unique: field_utils.format.datetime(this.recordData.__last_update).replace(/[^0-9]/g, ''),
+                    unique: field_utils.format.datetime(this.recordData.__last_update).replace(/[^0-9]/g, ''),
                 });
-                console.log(imgSrc);
             }
         }
-        
         //Click code, open in new popup in ORIGINAL size, minimum size popup 500x500 in css
         $($img).click(function(e) {
-/*           if(self.view.get("actual_mode") == "view") {
-                var $button = $(".oe_form_button_edit");
-                $button.openerpBounce();
-                e.stopPropagation();
-            }
-*/
-            // set attr SRC image, in our hidden div
-            //console.log("bla 2");
+
+/*        var activeAttachmentID = $(e.currentTarget).data('id');
+        var attachments = this.get('attachment_ids');
+        if (activeAttachmentID) {
+            var attachmentViewer = new DocumentViewer(this, attachments, activeAttachmentID);
+            attachmentViewer.appendTo($('body'));
+        }*/
+
+
+
             var a = $('#outer').find('img')[0]
             if (a) a.remove();
             $('#outer').prepend('<img id="inner" src="'+imgSrc+'" />');
@@ -102,24 +107,8 @@ imageWidget.include({
 
              }
            });
-            
-            
-        });
-            
-            
-        //!!!!!!!!!!!
-        //this.$('#outer').remove();
-        this.$('> img').remove();
-       // this.$('#inner').remove();
-        this.$el.prepend($img);
-        $img.on('error', function () {
-            self.on_clear();
-            $img.attr('src', self.placeholder);
-            self.do_warn(_t("Image"), _t("Could not display the selected image."));
         });
     },
-
-
+});
 });
 
-});
