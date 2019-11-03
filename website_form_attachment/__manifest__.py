891c2a59ced48195bfee37ba3e5e3dd52365e701
mega_buster <shurshilov.a@yandex.ru>
2019-11-04 00:08:25 +0300

    [FIX] template

--- website_form_attachment/__manifest__.py
+++ website_form_attachment/__manifest__.py
@@ -16,7 +16,7 @@
     'application': False,
     "auto_install": False,
     'installable': True,
-    'depends': ['website_form', 'website_crm', 'website_partner', 'crm'],
+    'depends': ['website_crm', 'crm'],
     'data': [
         'views/website_crm_templates.xml',
     ],
