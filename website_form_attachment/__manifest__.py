a934a39d9735de41c64404a6dacaa4a420b59fb5
mega_buster <shurshilov.a@yandex.ru>
2019-11-04 00:37:13 +0300

    [ADD] website attachment ver 13.0

---
+++ website_form_attachment/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Artem Shurshilov
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Website attachments contact us form',
+    'summary': '''Adds field attachments to contact us form. Attach contact website form attachment file form
+     files adds to website files form send file us files form''',
+    'version': '12.0.0.0.1',
+    'category': 'Tools',
+    'website': "http://www.eurodoo.com",
+    'author': 'Shurshilov Artem',
+    'license': 'LGPL-3',
+    #"price": 9.00,
+    #"currency": "EUR",
+    'application': False,
+    "auto_install": False,
+    'installable': True,
+    'depends': ['website_form', 'website_crm', 'website_partner', 'crm'],
+    'data': [
+        'views/website_crm_templates.xml',
+    ],
+    'images':[
+            'static/description/field.png',
+            'static/description/choose.png',
+            'static/description/result.png',
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+}
---
+++ website_form_attachment/models/crm.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Artem Shurshilov
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class Lead(models.Model):
+    _inherit = 'crm.lead'
+    attachment = fields.Binary(string='attachment', attachment=True)
