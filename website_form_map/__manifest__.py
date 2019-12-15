8af1948db39b8b02e4bcb6d549f6ef8a5b584c29
mega_buster <shurshilov.a@yandex.ru>
2019-12-15 21:24:41 +0300

    [ADD] leaflet.js map OSM

---
+++ website_form_map/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Artem Shurshilov
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Website contact us leaflet OSM free map',
+    'summary': '''Adds leaflet OSM map on contact us form''',
+    'version': '13.0.0.0.1',
+    'category': 'Tools',
+    'website': "http://www.eurodoo.com",
+    'author': 'Shurshilov Artem',
+    'license': 'LGPL-3',
+    #"price": 9.00,
+    #"currency": "EUR",
+    'application': False,
+    "auto_install": False,
+    'installable': False,
+    'depends': ['base','website','website_form', 'website_crm', 'website_partner', 'crm'],
+    'data': [
+        'views/website_crm_templates.xml',
+    ],
+    'images':[
+            'static/description/field.png',
+            'static/description/choose.png',
+            'static/description/result.png',
+    ],
+    "external_dependencies": {"python": [], "bin": []},
+}
