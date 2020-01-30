a21c3fb5f5a8e4f723e66a6950d583169f5ef019
shurshilov <shurshilov.a@yandex.ru>
2020-01-30 10:06:35 +0300

    [ADD] tags to manifest

--- web_image_webcam/__manifest__.py
+++ web_image_webcam/__manifest__.py
@@ -4,7 +4,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Web Widget - Image WebCam",
-    "summary": "Allows to take image with WebCam",
+    "summary": """Allows to take image with WebCam
+    [TAGS] web camera web foto web photo web images camera 
+    image snapshot web snapshot webcam snapshot picture web contact
+    image web product image online mobile web image produt mobile""",
     "version": "13.0.1.0.0",
     "category": "web",
     "website": "https://www.eurodoo.com",
