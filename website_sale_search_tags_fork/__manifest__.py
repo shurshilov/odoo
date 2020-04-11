0d8fb876de059e3f31a4ab269e586e89e1ae5068
mega_buster <shurshilov.a@yandex.ru>
2020-04-11 17:32:47 +0300

    [ADD] search by tags rename

---
+++ website_sale_search_tags_fork/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2020 Artem Shurshilov <shurshilov.a@yandex.ru>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Website Search Product Tags",
+
+    'summary': """
+        Search website products by tags such as categories good and complete search""",
+
+    'author': "Shurshilov Artem",
+    'website': "http://www.eurodoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'eCommerce',
+    'version': '13.0.0.0',
+    "license": 'LGPL-3',
+    'price': 29,
+    'currency': 'EUR',
+    'images': [
+        'static/description/search.png',
+        'static/description/product_tags.png',
+        'static/description/web_search_tags.png',
+    ],
+
+    # any module necessary for this one to work correctly
+    'depends': ['website_sale', 'product_tags_fork', 'stock'],
+    'installable': True,
+
+    # always loaded
+    # 'data': [
+    #     'views/views.xml',
+    # ],
+
+    # 'qweb': [
+    #     "static/src/xml/base.xml",
+    # ],
+}
---
+++ website_sale_search_tags_fork/controllers.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017-2020 Artem Shurshilov
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale as controller
+from odoo.osv import expression
+
+class WebsiteSale(controller):
+   def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
+        domains = [request.website.sale_product_domain()]
+        if search:
+            for srch in search.split(" "):
+                subdomains = [
+                    [('name', 'ilike', srch)],
+                    ['|',('product_variant_ids.default_code', 'ilike', srch),
+                    ('tag_ids', 'ilike', srch)]
+                ]
+                if search_in_description:
+                    subdomains.append([('description', 'ilike', srch)])
+                    subdomains.append([('description_sale', 'ilike', srch)])
+                domains.append(expression.OR(subdomains))
+
+        if category:
+            domains.append([('public_categ_ids', 'child_of', int(category))])
+
+        if attrib_values:
+            attrib = None
+            ids = []
+            for value in attrib_values:
+                if not attrib:
+                    attrib = value[0]
+                    ids.append(value[1])
+                elif value[0] == attrib:
+                    ids.append(value[1])
+                else:
+                    domains.append([('attribute_line_ids.value_ids', 'in', ids)])
+                    attrib = value[0]
+                    ids = [value[1]]
+            if attrib:
+                domains.append([('attribute_line_ids.value_ids', 'in', ids)])
+
+        return expression.AND(domains)
