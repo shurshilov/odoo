396ad95d84de248ac1ded058823a1b93a106cf8c
Artem <shurshilov.a@yandex.ru>
2021-11-20 19:20:41 +0300

    Update __manifest__.py

--- pos_chat_button/__manifest__.py
+++ pos_chat_button/__manifest__.py
@@ -25,7 +25,7 @@
     'summary': """Chat (chatter) In POS Screen""",
     'description': "This module adds chat interface in Point of sale screen.",
     'license': 'AGPL-3',
-    'author': "Artem Shurshilov",
+    'author': "Shurshilov Artem",
 #    'website': "https://vk.com/id20132180",
     'website': "http://www.eurodoo.com",
     'category': 'Point Of Sale',
