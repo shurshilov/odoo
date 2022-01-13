0712241577a67a0c10b83291f1c0b24d228e13cf
Artem Shurshilov <shurshilov.a@yandex.ru>
2022-01-13 22:20:50 +0300

    [MIG] web image webcam work on 15 version

--- web_image_webcam/__manifest__.py
+++ web_image_webcam/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
-# Copyright 2019 Shurshilov Artem <shurshilov.a@yandex.ru>
+# Copyright 2019-2022 Shurshilov Artem <shurshilov.a@yandex.ru>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Web Widget - Image WebCam",
@@ -8,7 +8,7 @@
     [TAGS] web camera web foto web photo web images camera 
     image snapshot web snapshot webcam snapshot picture web contact
     image web product image online mobile web image produt mobile""",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "web",
     "website": "https://www.eurodoo.com",
     "author": "Tech Receptives, "
@@ -17,19 +17,24 @@
               "Shurshilov Artem",
     "license": "LGPL-3",
     "price": 19.00,
-    'images':[
+    'images': [
             'static/description/field.png',
             'static/description/choose.png',
     ],
     "currency": "EUR",
-    "data": [
-        "views/assets.xml",
-    ],
     "depends": [
         "web",
     ],
-    "qweb": [
-        "static/src/xml/web_widget_image_webcam.xml",
-    ],
+
+    'assets': {
+        'web.assets_backend': [
+            'web_image_webcam/static/src/**/*.css',
+            'web_image_webcam/static/src/js/webcam.js',
+            'web_image_webcam/static/src/js/webcam_widget.js',
+        ],
+        'web.assets_qweb': [
+            'web_image_webcam/static/src/**/*.xml',
+        ],
+    },
     "installable": True,
 }
--- web_image_webcam/static/src/js/webcam_widget.js
+++ web_image_webcam/static/src/js/webcam_widget.js
@@ -1,25 +1,21 @@
 /*
     Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
-    Copyright 2019 Shurshilov Artem <shurshilov.a@yandex.ru>
+    Copyright 2019-2022 Shurshilov Artem <shurshilov.a@yandex.ru>
     License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 */
-odoo.define('web_image_webcam.webcam_widget', function(require) {
+odoo.define('web_image_webcam.webcam_widget', function (require) {
     "use strict";
 
     var core = require('web.core');
-//    var Model = require('web.Model');
     var Dialog = require('web.Dialog');
     var base_f = require('web.basic_fields');
     var imageWidget = base_f.FieldBinaryImage;
-    var DocumentViewer = require('mail.DocumentViewer');
-    var field_utils = require('web.field_utils');
-
     var _t = core._t;
     var QWeb = core.qweb;
 
     imageWidget.include({
 
-        _render: function () {
+        _render: async function () {
             this._super();
 
             var self = this,
@@ -28,35 +24,11 @@ odoo.define('web_image_webcam.webcam_widget', function(require) {
 
             // ::webcamjs:: < https://github.com/jhuckaby/webcamjs >
             // Webcam: Set Custom Parameters
-            Webcam.set({
-                width: 320,
-                height: 240,
-                dest_width: 320,
-                dest_height: 240,
-                image_format: 'jpeg',
-                jpeg_quality: 90,
-                force_flash: false,
-                fps: 45,
-                swfURL: '/web_image_webcam/static/src/js/webcam.swf',
-                //force_flash: true,
-            });
+            //let stream = await navigator.mediaDevices.getUserMedia({video: true});
+
 
             self.$el.find('.o_form_binary_file_web_cam').removeClass('col-md-offset-5');
-
-/*            new Model('ir.config_parameter').call('get_param', ['web_widget_image_webcam.flash_fallback_mode', false]).
-            then(function(default_flash_fallback_mode) {
-                if (default_flash_fallback_mode == 1) {
-                    Webcam.set({
-                        
-                            :: Important Note about Chrome 47+ :: < https://github.com/jhuckaby/webcamjs#important-note-for-chrome-47 >
-                            Setting "force_flash" to "true" will always run in Adobe Flash fallback mode on Chrome, but it is not desirable.
-                        
-                        force_flash: true,
-                    });
-                }
-            });*/
-
-            self.$el.find('.o_form_binary_file_web_cam').off().on('click', function(){
+            self.$el.find('.o_form_binary_file_web_cam').off().on('click', function () {
                 // Init Webcam
                 new Dialog(self, {
                     size: 'large',
@@ -67,10 +39,10 @@ odoo.define('web_image_webcam.webcam_widget', function(require) {
                         {
                             text: _t("Take Snapshot"), classes: 'btn-primary take_snap_btn',
                             click: function () {
-                                Webcam.snap( function(data) {
+                                Webcam.snap(function (data) {
                                     img_data = data;
                                     // Display Snap besides Live WebCam Preview
-                                    WebCamDialog.find("#webcam_result").html('<img src="'+img_data+'"/>');
+                                    WebCamDialog.find("#webcam_result").html('<img src="' + img_data + '"/>');
                                 });
                                 // Remove "disabled" attr from "Save & Close" button
                                 $('.save_close_btn').removeAttr('disabled');
@@ -105,8 +77,18 @@ odoo.define('web_image_webcam.webcam_widget', function(require) {
                         }
                     ]
                 }).open();
-                 console.log(WebCamDialog);
-                 console.log(WebCamDialog.find('#live_webcam'));
+                Webcam.set({
+                    width: 640,
+                    height: 480,
+                    // dest_width: width,
+                    // dest_height: height,
+                    image_format: 'jpeg',
+                    jpeg_quality: 100,
+                    force_flash: false,
+                    fps: 45,
+                    swfURL: '/web_image_webcam/static/src/js/webcam.swf',
+                    //force_flash: true,
+                });
                 Webcam.attach(WebCamDialog.find('#live_webcam')[0]);
 
                 // At time of Init "Save & Close" button is disabled
@@ -121,6 +103,7 @@ odoo.define('web_image_webcam.webcam_widget', function(require) {
     Dialog.include({
         destroy: function () {
             // Shut Down the Live Camera Preview | Reset the System
+            Webcam.off('live');
             Webcam.reset();
             this._super.apply(this, arguments);
         },
