0beb835d5d04429a63b6a63076df2e9ddcf21c1a
Artem Shurshilov <shurshilov.a@yandex.ru>
2022-02-05 19:29:51 +0300

    [ADD] center webmac qweb

--- web_image_webcam/__manifest__.py
+++ web_image_webcam/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Siddharth Bhalgami <siddharth.bhalgami@techreceptives.com>
-# Copyright 2019 Shurshilov Artem <shurshilov.a@yandex.ru>
+# Copyright 2019-2022 Shurshilov Artem <shurshilov.a@yandex.ru>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Web Widget - Image WebCam",
     "summary": "Allows to take image with WebCam",
-    "version": "12.0.1.0.0",
+    "version": "12.0.2.0.0",
     "category": "web",
     "website": "https://www.eurodoo.com",
     "author": "Tech Receptives, "
