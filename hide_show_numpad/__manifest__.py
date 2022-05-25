e56fc0bfd3d2b44ac0f2b487ae0cc65b1bb53e31
Artem Shurshilov <shurshilov.a@yandex.ru>
2022-05-25 21:51:13 +0300

    [MIG] hide show numpad

--- hide_show_numpad/__manifest__.py
+++ hide_show_numpad/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2020 Artem Shurshilov <shurshilov.a@yandex.ru>
+# Copyright (C) 2020-2022 Artem Shurshilov <shurshilov.a@yandex.ru>
 # License OPL-1.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Show/Hide NumPad POS",
@@ -14,11 +14,11 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Point Of Sale',
-    'version': '13.0.0.0',
+    'version': '15.0.0.0',
     "license": "LGPL-3",
     # 'price': 29,
     # 'currency': 'EUR',
-    'images':[
+    'images': [
         'static/description/button.png',
         'static/description/result.png',
         'static/description/result.png',
@@ -26,14 +26,17 @@
     ],
 
     # any module necessary for this one to work correctly
-    'depends': ['base','point_of_sale'],
+    'depends': ['base', 'point_of_sale'],
     'installable': True,
 
-    # always loaded
-    'data': [
-        'views/assets.xml',
-    ],
-
+    'assets': {
+        'web.assets_backend': [
+            'hide_show_numpad/static/src/js/hide_numpad.js',
+        ],
+        'web.assets_qweb': [
+            'hide_show_numpad/static/src/**/*.xml',
+        ],
+    },
     'qweb': [
         'static/src/xml/pos.xml',
     ],
--- hide_show_numpad/static/src/js/hide_numpad.js
+++ hide_show_numpad/static/src/js/hide_numpad.js
@@ -1,19 +1,17 @@
+
 odoo.define('hide_show_numpad', function (require) {
     "use strict";
-    var screens = require('point_of_sale.screens');
+    const ProductScreen = require('point_of_sale.ProductScreen');
+    const { patch } = require('web.utils');
 
-    screens.NumpadWidget.include({
-        renderElement: function () {
-            this._super();
-            $('.numpad-toggle').on('click', function () {
-                var self = this;
-                $(this).parent().siblings('.numpad-container').slideToggle(function () {
-                    $(self).toggleClass('fa-caret-down fa-caret-up');
-                    if($(this).is(':visible')){
-                        $('.order-scroller').animate({scrollTop: $('.order-scroller').height()}, 500);
-                    }
-                });
+    patch(ProductScreen.prototype, 'hide_show_numpad', {
+        toogleNumpad() {
+            $('.pads').slideToggle(() => {
+                $('.numpad-toggle').toggleClass('fa-caret-down fa-caret-up');
+                if ($(this).is(':visible'))
+                    $('.order-scroller').animate({ scrollTop: $('.order-scroller').height() }, 500);
             });
         }
-    });
+    })
+
 });
