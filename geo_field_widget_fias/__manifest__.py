86096f890c09db1ced045cbd9597a85b45160dae
Artem Shurshilov <shurshilov.a@yandex.ru>
2025-09-20 17:16:49 +0300

    [ADD] 18 version

--- geo_field_widget_fias/__manifest__.py
+++ geo_field_widget_fias/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Field Widget FIAS",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.1",
     "depends": ["web"],
     "author": "EURO ODOO, Shurshilov Artem",
     "maintainer": "EURO ODOO",
--- geo_mixin_product/__manifest__.py
+++ geo_mixin_product/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Geo Coordinates Yandex, NSPD, FIAS Widget for product",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.1",
     "depends": [
         "web",
         "product",
